45f6d73ad4152d2d90f160095f1b609921a402a6
Tobias Davis <tobias@davistobias.com>
2021-05-05 12:51:38 -0500

    starting a refactor to use Cloudflare Workers

--- rollup.config.js
+++ rollup.config.js
@@ -32,84 +32,107 @@ const production = !process.env.ROLLUP_WATCH
 // 	}
 // }
 
-export default [
-	{
-		input: 'client/main.js',
-		output: {
-			sourcemap: true,
-			format: 'iife',
-			dir: 'public/build'
-		},
-		plugins: [
-			svelte({
-				compilerOptions: {
-					// enable run-time checks when not in production
-					dev: !production
-				}
-			}),
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css({ output: 'bundle.css' }),
-
-			// If you have external dependencies installed from
-			// npm, you'll most likely need these plugins. In
-			// some cases you'll need additional configuration -
-			// consult the documentation for details:
-			// https://github.com/rollup/plugins/tree/master/packages/commonjs
-			resolve({
-				browser: true,
-				dedupe: [ 'svelte' ]
-			}),
-			commonjs(),
-
-			// Watch the `public` directory and refresh the
-			// browser on changes when not in production
-			!production && livereload('public'),
-
-			// If we're building for production (npm run build
-			// instead of npm run dev), minify
-			production && terser()
-		],
-		watch: {
-			clearScreen: false
-		}
+const client = {
+	input: 'client/main.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		dir: 'public/build'
 	},
-	{
-		input: production
-			? 'server/lambda.js'
-			: 'server/local-development.js',
-		output: {
-			sourcemap: true,
-			format: 'cjs',
-			exports: production
-				? 'default'
-				: undefined,
-			dir: 'deploy/build'
-		},
-		plugins: [
-			// If you have external dependencies installed from
-			// npm, you'll most likely need these plugins. In
-			// some cases you'll need additional configuration -
-			// consult the documentation for details:
-			// https://github.com/rollup/plugins/tree/master/packages/commonjs
-			resolve({
-				browser: false,
-				preferBuiltins: true
-			}),
-			string({
-				include: '**/*.md'
-			}),
-			commonjs(),
+	plugins: [
+		svelte({
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			}
+		}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({ output: 'bundle.css' }),
 
-			json(),
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true,
+			dedupe: [ 'svelte' ]
+		}),
+		commonjs(),
 
-			// In dev mode, call `npm run start` once
-			// the API bundle has been generated
-			!production && run(),
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('public'),
 
-			// If we're building for production (npm run build
-			// instead of npm run dev), minify
-			production && terser()
-		]
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
 	}
+}
+
+const lambda = {
+	input: production
+		? 'server/lambda.js'
+		: 'server/local-development.js',
+	output: {
+		sourcemap: true,
+		format: 'cjs',
+		exports: production
+			? 'default'
+			: undefined,
+		dir: 'deploy/build'
+	},
+	plugins: [
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: false,
+			preferBuiltins: true
+		}),
+		string({
+			include: '**/*.md'
+		}),
+		commonjs(),
+
+		json(),
+
+		// In dev mode, call `npm run start` once
+		// the API bundle has been generated
+		!production && run(),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	]
+}
+
+const cloudflareApi = {
+	input: '_app/cloudflare-api/worker-wrapped.js',
+	output: {
+		sourcemap: true,
+		format: 'es',
+		file: '_deploy/cloudflare-api/build.js'
+	},
+	plugins: [
+		resolve({
+			browser: true
+		}),
+		commonjs()
+	],
+	watch: {
+		clearScreen: false
+	}
+}
+
+export default [
+	// client,
+	// lambda,
+	cloudflareApi
 ]
