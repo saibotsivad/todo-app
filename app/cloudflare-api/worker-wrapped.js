4d4ffbb8bc276abef27de0020299832cbf3c9758
Tobias Davis <tobias@davistobias.com>
2021-05-19 10:54:11 -0500

    handle JSON write requests to CF Workers

--- app/cloudflare-api/worker-wrapped.js
+++ app/cloudflare-api/worker-wrapped.js
@@ -1,32 +1,39 @@
+import { BadRequest } from '@/lib/exceptions.js'
 import { setupRouter } from './setup-router.js'
 import { dynamodb } from '@/service/db.js'
 import { routeRequest } from '@/lib/route-request.js'
 import { ksuid } from '@/lib/ksuid.js'
+import errorFormatter from '@/lib/error-formatter.js'
 import log from '@/service/log.js'
 import remapPairsToMap from '@/lib/remap-pairs-to-map.js'
 import Trouter from 'trouter'
-
-// set('AWS_ACCOUNT_ID', AWS_ACCOUNT_ID)
-// set('AWS_REGION', AWS_REGION)
-// set('AWS_ACCESS_KEY_ID', AWS_ACCESS_KEY_ID)
-// set('AWS_SECRET_ACCESS_KEY', AWS_SECRET_ACCESS_KEY)
-// set('TJ_TABLE_NAME', TJ_TABLE_NAME)
-// set('TJ_API_DOMAIN', TJ_API_DOMAIN)
-// set('NODE_ENV', 'production')
-// set('IS_DEPLOYED', 'true')
 
 // eslint-disable-next-line no-undef
 addEventListener('fetch', event => {
 	event.respondWith(handleRequest(event.request))
 })
 
+const supportedWriteMethods = [
+	'POST',
+	'PATCH',
+]
+
 let router
+
+const errorHandler = error => new Response(JSON.stringify({
+	errors: [ errorFormatter(error) ],
+}), {
+	status: 400,
+	headers: new Headers({
+		'content-type': 'application/json',
+	}),
+})
 
 async function handleRequest(req) {
 	if (!router) {
 		// The configuration settings are loaded once per instantiation, so if you
 		// change them midway you'll have to wait until the instances go away.
-		// eslint-disable-next-line no-undef
+		/* global TODO_JOURNAL_CONFIGURATION */
 		const options = JSON.parse(await TODO_JOURNAL_CONFIGURATION)
 		const config = { get: key => options[key] }
 		router = new Trouter()
@@ -39,10 +46,21 @@ async function handleRequest(req) {
 		headers: remapPairsToMap(req.headers),
 		pathname: url.pathname,
 		search: url.search,
-		body: req.body, // TODO
 	}
 	if (request.search) {
 		request.query = remapPairsToMap(url.searchParams)
+	}
+
+	if (supportedWriteMethods.includes(request.method)) {
+		if ((req.headers.get('content-type') || '').includes('application/json')) {
+			try {
+				request.body = await req.json()
+			} catch (error) {
+				return errorHandler(new BadRequest('Could not parse JSON body of request. Badly formatted JSON string?'))
+			}
+		} else {
+			return errorHandler(new BadRequest('Write requests currently only accept JSON, and must have the Content-Type header set.'))
+		}
 	}
 
 	const start = Date.now()
