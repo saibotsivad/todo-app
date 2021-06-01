f7ca5cf61ee8dc2d6d9bb03d4749ccc656efab21
Tobias Davis <tobias@davistobias.com>
2021-06-01 14:11:00 -0500

    replace snarkdown with remarkable for more control

--- app/cloudflare-api/_lib/controller/email-template/send-email-template.js
+++ app/cloudflare-api/_lib/controller/email-template/send-email-template.js
@@ -1,9 +1,11 @@
 import { UnexpectedServiceResponse } from '@/lib/exceptions.js'
 import { getEmailTemplate } from '@/lib/controller/email-template/get-email-template.js'
-import snarkdown from 'snarkdown'
+import { Remarkable } from 'remarkable'
 import templite from 'templite'
 
 const Charset = 'UTF-8'
+
+let md
 
 export const sendEmailTemplate = async (services, { fromAddress, toAddress, subject, templateId, parameters }) => {
 	const { email, log } = services
@@ -13,8 +15,26 @@ export const sendEmailTemplate = async (services, { fromAddress, toAddress, subj
 		throw new Error(`Could not locate template by id "${templateId}", this is a developer error.`)
 	}
 
+	if (!md) {
+		md = new Remarkable({
+			// Set to true to enable HTML tags in the source markdown
+			html: true,
+			// Set to true to use '/' to close single tags (<br />)
+			xhtmlOut: false,
+			// Set to true to convert '\n' in paragraphs into <br>
+			breaks: false,
+			// CSS language prefix for fenced blocks
+			langPrefix: 'language-',
+			// Enable some language-neutral replacement + quotes beautification
+			typographer: true,
+			// Double + single quotes replacement pairs, when typographer enabled,
+			// and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
+			quotes: '“”‘’',
+		})
+	}
+
 	const markdown = templite(emailTemplate.attributes.view, parameters)
-	const html = snarkdown(markdown)
+	const html = md.render(markdown)
 
 	const { success, data, response } = await email('SendEmail', {
 		Destination: { ToAddresses: [ toAddress ] },
